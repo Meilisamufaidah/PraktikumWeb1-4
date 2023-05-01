f16b4abd38caee4bd272af6f9b858588710623b0
Meilisamufaidah <128033146+Meilisamufaidah@users.noreply.github.com>
2023-05-01 13:59:27 +0700

    Add files via upload

---
+++ PraktikumWeb1-4/oop-praktikum-1/app/Method.php
@@ -0,0 +1,13 @@
+<?php
+ class Mahasiswa {
+ // Method
+ public function setNim(string $nim) {
+ return $nim;
+ }
+ public function setNama(string $nama) {
+ return $nama;
+ }
+ public function setUmur(int $umur) {
+ return $umur;
+ }
+ }
